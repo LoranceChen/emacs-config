39adb6ed2506f25073ca44244df6e3f6b35afd28
LoranceChen <wangchen@qiaobutang.com>
2016-12-02 23:41:46 +0800

    update

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
 (require 'package)
 (package-initialize)
 (setq package-archives '(("gnu"   . "https://elpa.zilongshanren.com/gnu/")
-			 ("melpa-stable" . "https://elpa.zilongshanren.com/melpa-stable/")))
+			 ("melpa" . "https://elpa.zilongshanren.com/melpa/")))
 
 ;(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
  ;                       ("marmalade" . "http://marmalade-repo.org/packages/")
@@ -21,7 +21,7 @@
    ;                     ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-; (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+;;(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 ;(add-to-list 'load-path "~/.emacs.d/org/") todo remove
 
 ;;add package if not installed
@@ -39,6 +39,7 @@
 		      ghc ; ghc-mode - sub mode of haskell-mode 
 		      company-ghc
 		      hindent ; haskell indent code
+		      ;;company-ghci
 		   ) "Default packages")
 
 (defun my-package-installed-p ()
@@ -137,14 +138,7 @@
 (require 'haskell-mode)
 (define-key haskell-mode-map (kbd "<f8>") 'haskell-navigate-imports)
 
-(autoload 'ghc-init "ghc" nil t)
-(autoload 'ghc-debug "ghc" nil t)
-(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
 
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-
-;;(remove-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 ;;interactive mode
 (require 'haskell-interactive-mode)
 (require 'haskell-process)
@@ -161,14 +155,8 @@
   (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
   (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
   (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
-;;inferior haskell mode - replaced by interactive haskell mode
-;;(require 'inf-haskell)
-
-;;(define-key haskell-mode-map (kbd "M-.") 'haskell-mode-jump-to-def)
-(define-key haskell-mode-map (kbd "C-c ,") 'pop-tag-mark)
-
-(setq haskell-tags-on-save t)
-;;(add-hook 'after-save-hook '(haskell-process-reload))
+;;; To get completions in the REPL
+;;(add-hook 'haskell-interactive-mode-hook 'company-mode)
 
 ;;ghc-mod
 (let ((my-cabal-path (expand-file-name "~/.cabal/bin")))
@@ -177,11 +165,30 @@
 (require 'company)
 (add-hook 'haskell-mode-hook 'company-mode)
 (add-to-list 'company-backends 'company-ghc)
-(custom-set-variables '(company-ghc-show-info t))
+;; TODO close-ghc
+(autoload 'ghc-init "ghc" nil t)
+(autoload 'ghc-debug "ghc" nil t)
+(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
 
-;;hindent
-(require 'hindent)
-(add-hook 'haskell-mode-hook #'hindent-mode)
+(remove-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
+
+;;inferior haskell mode - replaced by interactive haskell mode
+;;(require 'inf-haskell)
+
+;;TODO (require 'company-ghci)
+;;(push 'company-ghci company-backends)
+;;(add-hook 'haskell-mode-hook 'company-mode)
+;; To get completions in the REPL
+;;(add-hook 'haskell-interactive-mode-hook 'company-mode)
+
+;;(define-key haskell-mode-map (kbd "M-.") 'haskell-mode-jump-to-def)
+(define-key haskell-mode-map (kbd "C-c ,") 'pop-tag-mark)
+
+(setq haskell-tags-on-save t)
+;;(add-hook 'after-save-hook (lambda () (haskell-process-reload)))
+
 
 
 (custom-set-variables
@@ -196,11 +203,19 @@
    (quote
     ("f81a9aabc6a70441e4a742dfd6d10b2bae1088830dc7aba9c9922f4b1bd2ba50" default)))
  '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-load-or-reload-prompt t)
  '(haskell-process-log t)
  '(haskell-process-suggest-remove-import-lines t)
  '(package-selected-packages
    (quote
     (hungry-delete monokai-theme markdown-mode zygospore yasnippet ws-butler volatile-highlights undo-tree smartparens intero iedit helm-swoop helm-projectile helm-gtags ggtags function-args dtrt-indent company-irony comment-dwim-2 anzu))))
+
+;;hindent
+(require 'hindent)
+(add-hook 'haskell-mode-hook #'hindent-mode)
+
+
+
 
 
 
@@ -215,11 +230,11 @@
 ;;			    (interactive)
 ;;			    (other-frame 0)))
 
-(add-to-list 'company-backends 'company-ghc)
-
-(define-key haskell-mode-map (kbd "M-.") 'haskell-mode-goto-loc)
+(define-key haskell-mode-map (kbd "M-.") 'haskell-mode-jump-to-def-or-tag)
 (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-mode-show-type-at)
-;;(setenv "PATH" (concat (getenv "PATH") ":/Users/lorancechen/project/haskell/test1126/my-project"))
+
+;;todo put to project emcas config
+(setenv "PATH" (concat (getenv "PATH") ":~/project/haskell/test1126/my-project"))
 ;;===================
 
 ;;auto generated======
